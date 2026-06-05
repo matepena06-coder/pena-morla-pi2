737d911c68b9e103b421a0e19f1422dd3ae8da64
mateo-pena <matepena06@gmail.com>
2026-06-05 10:58:20 -0300

    Proyecto Base

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import app from "firebase/app"
+import firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSX7BozeMge8BuvRmqQsi71EUzyF_H28s",
+  authDomain: "pi-pena-morla-2.firebaseapp.com",
+  projectId: "pi-pena-morla-2",
+  storageBucket: "pi-pena-morla-2.firebasestorage.app",
+  messagingSenderId: "42076583863",
+  appId: "1:42076583863:web:bc99e36ddb3ce4ddaa1a32"
+};
+
+app.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const db = app.firebase()
